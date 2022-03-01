55d04af95e81b2b7f5cdd00ac1fb954861e62b26
Jorge Ktch <jorgektch@gmail.com>
2022-03-01 11:29:33 -0500

    Retos de funciones

--- semana-04/01-funciones-en-javascript-y-movimientos-2d/js/codigo.js
+++ semana-04/01-funciones-en-javascript-y-movimientos-2d/js/codigo.js
@@ -27,7 +27,70 @@ console.log(areaCuadrado(7));
 console.log(areaCuadrado(3));
 
 /*
+Reto 01:
 Calcular el area de un triangulo dadas la base y la altura (base, altura)
 y de un rectangulo de lados diferentes (base, altura)
 02 funciones
 */
+function areaTriangulo(base, altura){
+	let area = base*altura/2;
+	return area;
+}
+function areaRectangulo(base, altura){
+	let area = base*altura;
+	return area;
+}
+console.log(areaTriangulo(3,4));
+console.log(areaRectangulo(3,4));
+
+/*
+Reto 02:
+Dados w,h que son dimensiones de una lienzo
+canvas y x,y son posiciones
+y de un punto en 2d
+Crear una funcion que reciba todos esos datos
+y retorne true, si el punto esta dentro del
+lienzo y false si no lo esta
+*/
+function puntoInterior(x,y,w,h){
+	let estaDentro; /* Para almacenar el valor de verdad */
+	if(0<=x && x<w && 0<=y && y<h){
+		estaDentro = true;
+	}else{
+		estaDentro = false;
+	}
+	return estaDentro;
+}
+console.log(puntoInterior(1000,1000,1000,500));
+/*
+Reto 03:
+Dados w,h que son dimensiones de una lienzo
+canvas y x,y y wp,hp que son posiciones
+y dimensiones de un personaje en 2d
+Crear una funcion que reciba todos esos datos
+y retorne true, si la figura esta dentro del
+lienzo y false si no lo esta
+*/
+function figuraInterior(x,y,wp,hp,w,h){
+	let x2 = x+wp;
+	let y2 = y;
+
+	let x3 = x+wp;
+	let y3 = y+hp;
+
+	let x4 = x;
+	let y4 = y+hp;
+
+	let estaDentro; /* Para almacenar el valor de verdad */
+	if(puntoInterior(x,y,w,h) &&
+	   puntoInterior(x2,y2,w,h) &&
+	   puntoInterior(x3,y3,w,h) &&
+	   puntoInterior(x4,y4,w,h)){
+		estaDentro = true;
+	}else{
+		estaDentro = false;
+	}
+	return estaDentro;
+}
+
+console.log(figuraInterior(500,250,1000,500,1000,500));
