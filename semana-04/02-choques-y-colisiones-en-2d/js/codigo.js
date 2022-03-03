0d4733bb053f8a07c08676aa4f7fe4bf6d584a01
Jorge Ktch <jorgektch@gmail.com>
2022-03-03 12:04:11 -0500

    colision en 2d

--- semana-04/02-choques-y-colisiones-en-2d/js/codigo.js
+++ semana-04/02-choques-y-colisiones-en-2d/js/codigo.js
@@ -7,9 +7,9 @@ y retorne true si se intersecta
 y false si no lo hacen
 */
 
-function verificarInterseccion(li1, ls1, li2, ls2){
+function verificarInterseccion1D(li1, ls1, li2, ls2){
 	let li_min, ls_max;
-	
+
 	/* El valor minimo entre li1 y li2
 	li_min = Math.min(li1, li2); */
 	if(li1<li2){
@@ -33,5 +33,54 @@ function verificarInterseccion(li1, ls1, li2, ls2){
 	}
 }
 
-console.log(verificarInterseccion(-2,1,0,3));
-console.log(verificarInterseccion(0,3,4,7));
+console.log(verificarInterseccion1D(-2,1,0,3));
+console.log(verificarInterseccion1D(0,3,4,7));
+
+/*
+Se tienen las coordenadas y dimensiones de
+02 rectangulos ubicados en un lienzo canvas
+p: (xp,yp), (wp,hp)
+e: (xe,ye), (we,he)
+Se requiere implementar una funcion
+que retorne true si los rectangulos se
+interceptan o se tocan en alguno(s) de
+sus bordes; en caso contrario se debe
+retornar false
+*/
+
+function verificarInterseccion2D(xp,yp,wp,hp,xe,ye,we,he) {
+	let xinf = Math.min(xp,xe);
+	let yinf = Math.min(yp,ye);
+
+	let xsup = Math.max(xp+wp,xe+we);
+	let ysup = Math.max(yp+hp,ye+he);
+
+	let areap = wp*hp;
+	let areae = we*he;
+	let areat = (xsup-xinf)*(ysup-yinf);
+
+	let xc1;
+	if(xinf == xp){
+		xc1 = xp+wp;
+	}else{
+		xc1 = xe+we;
+	}
+	let areac1 = (xsup-xc1)*(ysup-yinf);
+
+	let yc2;
+	if(yinf == yp){
+		yc2 = yp+hp;
+	}else{
+		yc2 = ye+he;
+	}
+	let areac2 = (xsup-xinf)*(ysup-yc2);
+
+	if(areat<=areap+areae+areac1+areac2){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+console.log(verificarInterseccion2D(10,10,20,30,40,40,20,30));
+console.log(verificarInterseccion2D(10,10,20,30,30,40,20,30));
